67cdecbddc6c4a49430ac7d36fcbba618608b3f4
Justin Ho <justintingfung@gmail.com>
2017-12-12 11:07:38 +0800

    Created dog-routes

---
+++ routes/dog-routes.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const bodyParser = require('body-parser');
+const Dog = require('../models/dog-model')
+
+router.use(bodyParser.json());
+
+// Get the view of all dogs
+router.get('/view/:segment', (req, res) => {
+    Dog.find({}, {__v: 0}).limit(9).skip(req.params.segment*9).then(dogs => {
+        res.json(dogs);
+    });
+});
+
+// Post a dog registration
+router.post('/registration', (req, res) => {
+    Dog.create({
+        name: req.body.name,
+        breed: req.body.breed,
+        sex: req.body.sex,
+        age: req.body.age,
+        size: req.body.size,
+        description: req.body.description,
+        photos: req.body.photos,
+        ownerID: req.body.ownerID // For postman testing only, for real case use the code below
+        //ownerID: req.session.passport.user
+    });
+    res.send('New dog registered');
+});
+
+// Delete a dog registration
+router.delete('/registration/:dogID', (req, res) => {
+    Dog.remove({_id: req.params.dogID})
+        .then(()=>{
+            res.send('Registration deleted')
+        })
+})
+
+// Update a dog registration information
+router.put('/registration/:dogID', (req, res) => {
+    Dog.update(
+        {_id: req.params.dogID},
+        {
+            name: req.body.name,
+            breed: req.body.breed,
+            sex: req.body.sex,
+            age: req.body.age,
+            size: req.body.size,
+            description: req.body.description,
+            photos: req.body.photos
+        }
+    ).then(() => {
+        res.send('Dog information updated')
+    })
+})
+
+module.exports = router;
